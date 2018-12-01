7458ec584dfbb875fe91f53cab81b742e199d9a9
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-01 17:04:06 +0000

    using new DisplayBeers method instead of displaying by abv

--- src/views/abv_display_view.js
+++ src/views/abv_display_view.js
@@ -1,8 +1,8 @@
 const PubSub = require("../helpers/pub_sub.js");
+const DisplayBeers = require("./display_beers.js")
 
 const AbvDisplayView = function (displayArea) {
  this.displayArea = displayArea;
- this.beersByAbv = null;
 };
 
 
@@ -13,58 +13,84 @@ AbvDisplayView.prototype.bindEvents = function () {
   })
 };
 
+
 AbvDisplayView.prototype.displayBeers = function (beers) {
-  this.clearList()
-  this.loopThroughBeers(beers)
+  const displayBeers = new DisplayBeers(this.displayArea);
+  displayBeers.displayBeers(beers);
 };
-
-AbvDisplayView.prototype.loopThroughBeers = function (beers) {
-  beers.forEach((beer) => {
-    this.render(beer);
-  })
-};
-
-AbvDisplayView.prototype.clearList = function () {
-  this.displayArea.innerHTML = '';
-};
-
-AbvDisplayView.prototype.render = function (beer) {
-  const div = document.createElement('div');
-  div.className = "beer";
-  const beerName = this.customCreateElement('h2', "textContent", beer.name);
-  div.appendChild(beerName);
-  const beerTagline = this.customCreateElement('h6', "textContent", beer.tagline);
-  div.appendChild(beerTagline);
-  const picture = this.customCreateElement('img', "src", beer.image_url);
-  div.appendChild(picture);
-  const abv = this.customCreateElement('p', "textContent", `Abv: ${beer.abv}%`)
-  div.appendChild(abv);
-  const maltList = this.getMaltList(beer.ingredients.malt);
-  div.appendChild(maltList);
-  const twist = this.customCreateElement('p', "textContent", `Twist: ${beer.method.twist}`);
-  div.appendChild(twist);
-
-  this.displayArea.appendChild(div)
-};
-
-AbvDisplayView.prototype.getMaltList = function (malts) {
-  const maltList = this.customCreateElement("ul", "textContent", "Malts:");
-  malts.forEach((malt) => {
-    const newMalt = this.customCreateElement("li", "textContent", malt.name)
-    maltList.appendChild(newMalt)
-  })
-  return maltList;
-};
-
-
-AbvDisplayView.prototype.customCreateElement = function (type, value, content) {
-  const element = document.createElement(type)
-  element[value] = content;
-  return element;
-};
-
-
-
 
 
 module.exports = AbvDisplayView;
+
+
+//
+// const PubSub = require("../helpers/pub_sub.js");
+//
+// const AbvDisplayView = function (displayArea) {
+//  this.displayArea = displayArea;
+//  this.beersByAbv = null;
+// };
+//
+//
+// AbvDisplayView.prototype.bindEvents = function () {
+//   PubSub.subscribe('Beer:selected-beer-abv', (event) => {
+//     const selectedBeers = event.detail;
+//     this.displayBeers(selectedBeers)
+//   })
+// };
+//
+// AbvDisplayView.prototype.displayBeers = function (beers) {
+//   this.clearList()
+//   this.loopThroughBeers(beers)
+// };
+//
+// AbvDisplayView.prototype.loopThroughBeers = function (beers) {
+//   beers.forEach((beer) => {
+//     this.render(beer);
+//   })
+// };
+//
+// AbvDisplayView.prototype.clearList = function () {
+//   this.displayArea.innerHTML = '';
+// };
+//
+// AbvDisplayView.prototype.render = function (beer) {
+//   const div = document.createElement('div');
+//   div.className = "beer";
+//   const beerName = this.customCreateElement('h2', "textContent", beer.name);
+//   div.appendChild(beerName);
+//   const beerTagline = this.customCreateElement('h6', "textContent", beer.tagline);
+//   div.appendChild(beerTagline);
+//   const picture = this.customCreateElement('img', "src", beer.image_url);
+//   div.appendChild(picture);
+//   const abv = this.customCreateElement('p', "textContent", `Abv: ${beer.abv}%`)
+//   div.appendChild(abv);
+//   const maltList = this.getMaltList(beer.ingredients.malt);
+//   div.appendChild(maltList);
+//   const twist = this.customCreateElement('p', "textContent", `Twist: ${beer.method.twist}`);
+//   div.appendChild(twist);
+//
+//   this.displayArea.appendChild(div)
+// };
+//
+// AbvDisplayView.prototype.getMaltList = function (malts) {
+//   const maltList = this.customCreateElement("ul", "textContent", "Malts:");
+//   malts.forEach((malt) => {
+//     const newMalt = this.customCreateElement("li", "textContent", malt.name)
+//     maltList.appendChild(newMalt)
+//   })
+//   return maltList;
+// };
+//
+//
+// AbvDisplayView.prototype.customCreateElement = function (type, value, content) {
+//   const element = document.createElement(type)
+//   element[value] = content;
+//   return element;
+// };
+//
+//
+//
+//
+//
+// module.exports = AbvDisplayView;
